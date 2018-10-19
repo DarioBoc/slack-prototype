cfd6c517cb464d0bf0e5b025c0e23011ba754ead
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-10-18 22:31:48 -0300

    Fix wrong format file

--- backend/resolvers/team.js
+++ backend/resolvers/team.js
@@ -1,13 +1,12 @@
 export default {
-    Mutation: {
-        createTeam: async (parent, args, { models, user }) => {
-            try {
-                await models.Team.create({ ...args, owner: user.id })
-                return true;
-            } catch (error) {
-                console.log(error);
-                return false;
-            }
-        }
+  Mutation: {
+    createTeam: async (parent, args, { models, user }) => {
+      try {
+        await models.Team.create({ ...args, owner: user.id });
+        return true;
+      } catch (error) {
+        return false;
+      }
     },
+  },
 };
