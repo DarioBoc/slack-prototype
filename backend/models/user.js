2198c975af0a023a30a91f643d6cd468168eb1ce
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-10-18 22:19:55 -0300

    Add validation to the user entity

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,51 @@
+import bcrypt from 'bcrypt';
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     username: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'The username can only contain letters and numbers',
+        },
+        len: {
+          args: [3, 20],
+          msg: 'The username needs to be between 3 and 20 characters long',
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'The email is invalid',
+        },
+      },
     },
-    password: DataTypes.STRING,
-  }, { underscored: true });
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [5, 20],
+          msg: 'The password needs to be between 5 and 20 characters long',
+        },
+      },
+    },
+  },
+  {
+    underscored: true,
+    hooks: {
+      afterValidate: async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 12);
+        // eslint-disable-next-line no-param-reassign
+        user.password = hashedPassword;
+      },
+    },
+  });
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
