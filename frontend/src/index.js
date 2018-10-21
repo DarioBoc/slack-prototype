3babb19720d739dfcfc2a311877b6e6ffd84b4cf
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-10-20 21:53:25 -0300

    Remove unused vars

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import gql from "graphql-tag";
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from "apollo-boost";
 
@@ -9,13 +8,13 @@ import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000/"
+  uri: "http://localhost:4000/"
 });
 
 const App = (
-    <ApolloProvider client={client}>
-        <Routes />
-    </ApolloProvider>
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
 );
 
 ReactDOM.render(App, document.getElementById('root'));
--- frontend/src/routes/home.js
+++ frontend/src/routes/home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Query } from "react-apollo";
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
