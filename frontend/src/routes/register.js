7d40a23903fef45bc4c3e32b764d2f84c6583f38
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-10-20 21:28:00 -0300

    Add register component

--- frontend/src/routes/register.js
+++ frontend/src/routes/register.js
@@ -1,67 +1,104 @@
 import React from 'react';
-import { Button, Input, Container, Header } from 'semantic-ui-react';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Message, Button, Input, Container, Header } from 'semantic-ui-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-class Register extends React.Component {
-    state = {
-        username: '',
-        email: '',
-        password: '',
-    };
+const registerObserver = observer(class Register extends React.Component {
+  constructor(props) {
+    super(props);
 
-    onSubmit = async () => {
-        const response = await this.props.mutate({
-          variables: this.state,
-        });
-    
-        console.log(response);
-    };
+    extendObservable(this, {
+      username: '',
+      usernameError: '',
+      email: '',
+      emailError: '',
+      password: '',
+      passwordError: '',
+    });
+  }
+
+  onSubmit = async () => {
+    Object.assign(this, {
+      usernameError: '',
+      emailError: '',
+      passwordError: '',
+    });
+
+    const {username, email, password} = this;
+
+    const response = await this.props.mutate({
+      variables: {username, email, password},
+    });
+
+    const {ok, errors} = response.data.register;
+
+    if (ok) {
+      this.props.history.push('/');
+    } else {
+      errors.forEach(({path, message}) => {
+        this[`${path}Error`] = message;
+      });
+    }
+  };
 
     onChange = e => {
-        const { name, value } = e.target;
-        // name = "email";
-        this.setState({ [name]: value });
+      const { name, value } = e.target;
+      // name = "email";
+      this[name] = value;
     };
 
     render() {
-        const { username, email, password } = this.state;
+      const { username, email, password, usernameError, emailError, passwordError } = this;
 
-        return (
-            <Container text>
-                <Header as="h2">Register</Header>
-                    <Input
-                        name="username"
-                        onChange={this.onChange}
-                        value={username}
-                        placeholder="Username"
-                        fluid
-                    />
-                    <Input
-                        name="email"
-                        onChange={this.onChange}
-                        value={email}
-                        placeholder="Email"
-                        fluid 
-                    />
-                    <Input
-                        name="password"
-                        onChange={this.onChange}
-                        value={password}
-                        type="password"
-                        placeholder="Password"
-                        fluid
-                    />
-                    <Button onClick={this.onSubmit}>Submit</Button>
-                </Container>
-        );
+      return (
+        <Container text>
+          <Header as="h2">Register</Header>
+          <Input
+            error={!!usernameError}
+            name="username"
+            onChange={this.onChange}
+            value={username}
+            placeholder="Username"
+            fluid
+          />
+          <Input
+            error={!!emailError}
+            name="email"
+            onChange={this.onChange}
+            value={email}
+            placeholder="Email"
+            fluid 
+          />
+          <Input
+            error={!!passwordError}
+            name="password"
+            onChange={this.onChange}
+            value={password}
+            type="password"
+            placeholder="Password"
+            fluid
+          />
+          <Button onClick={this.onSubmit}>Submit</Button>
+          {usernameError || emailError || passwordError ? (
+            <Message error header="There was some errors with your submission" list={[usernameError, emailError, passwordError].filter(x => x)} />
+          ) : null}
+        </Container>
+      );
     }
-}
+});
 
 const registerMutation = gql`
   mutation($username: String!, $email: String!, $password: String!) {
-    register(username: $username, email: $email, password: $password)
+    register(username: $username, email: $email, password: $password) {
+      ok,
+      errors {
+        path,
+        message,
+      }
+    }
   }
 `;
 
-export default graphql(registerMutation)(Register);
+export default graphql(registerMutation)(registerObserver);
