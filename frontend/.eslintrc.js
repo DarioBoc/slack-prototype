688922c9f0889ce4b3541e6d6e7b8c636f0d8248
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2018-10-20 21:23:53 -0300

    Update React eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
-    extends: 'airbnb',
-    plugins: ['react', 'jsx-a11y', 'import'],
-    rules: {
-      'react/jsx-filename-extension': 0,
-    },
-    globals: {
-      document: 1,
-    },
-  };
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'indent': ['error', 2],
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0,
+  },
+  globals: {
+    document: 1,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      'jsx': true,
+      'modules': true
+    }
+  },
+};
